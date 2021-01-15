82322fe58b7c47a7090c53ef84fc2415e7d5df7c
Anton Vo <S300938@oslomet.no>
2021-01-15 12:59:29 +0100

    Added button defs

--- embedded/samples/simple_button/src/main.c
+++ embedded/samples/simple_button/src/main.c
@@ -5,27 +5,79 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME 	1000
 
+#define LED_2 3
+#define LED_3 4
+#define button_1 6
+#define button_2 7
+#define switch_1 8
+#define switch_2 9
+
+
+
+
+static struct gpio_callback cb;
+const struct device *dev;
+
+int button_1_state = 2;
+int button_2_state = 2;
+int switch_1_state = 2;
+int switch_2_state = 2;
+
+void button1_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins) {
+	button_1_state = gpio_pin_get(dev, button_1);
+	button_2_state = gpio_pin_get(dev, button_2);
+
+	switch_1_state = gpio_pin_get(dev, switch_1);
+	switch_2_state = gpio_pin_get(dev, switch_2);
+
+	printk("Button 1: %d  Button 2: %d  Switch 1: %d  Switch 2: %d\n\n", button_1_state, button_2_state, switch_1_state, switch_2_state);
+}
+
 void main(void)
 {
-	int cnt = 0;
-	struct device *dev;
+	printk("\n\n\n\n\n\n\n*** Running Simple Sample ***");
+
+
+	int count = 0;
+
+	
 
 	dev = device_get_binding("GPIO_0");
+
 	/* Set LED pin as output */
-	gpio_pin_configure(dev, 3, GPIO_OUTPUT); //p0.03 == LED2
-	gpio_pin_configure(dev, 4, GPIO_OUTPUT); //p0.04 == LED3
-	gpio_pin_configure(dev, 17, GPIO_OUTPUT); //p0.17
+	gpio_pin_configure(dev, LED_2, GPIO_OUTPUT);
+	gpio_pin_configure(dev, LED_3, GPIO_OUTPUT);
+
+	/* Set Button as input */
+	gpio_pin_configure(dev, button_1, GPIO_INPUT);
+	gpio_pin_interrupt_configure(dev, button_1, GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_init_callback(&cb, button1_pressed, BIT(button_1));
+	gpio_add_callback(dev, &cb);
+
+	gpio_pin_configure(dev, button_2, GPIO_INPUT);
+	gpio_pin_interrupt_configure(dev, button_2, GPIO_INT_EDGE_TO_ACTIVE);
+
+	/* Set Switch as input */
+	gpio_pin_configure(dev, switch_1, GPIO_INPUT);
+	gpio_pin_configure(dev, switch_2, GPIO_INPUT);
+
 
 	while (1) {
+
 		/* Set pin to HIGH/LOW every 1 second */
-        if(cnt % 2 == 0)
+        if(count % 2 == 0)
         {
-            gpio_pin_toggle(dev, 3);	//p0.03 == LED2
-		    gpio_pin_toggle(dev, 4);	//p0.04 == LED3
-		    gpio_pin_toggle(dev, 17);
+            gpio_pin_toggle(dev, LED_2);	
+		    gpio_pin_toggle(dev, LED_3);
+
+			
+
         }
-			//p0.17 Toggling pin 17
-		cnt++;
+
+		count++;
 		k_msleep(SLEEP_TIME);
+
+
 	}
 }
