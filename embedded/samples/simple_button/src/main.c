3b5d55d671d4c98bba47b2b959f119ab99eed05c
einatomter <55589561+einatomter@users.noreply.github.com>
2021-01-15 15:25:33 +0100

    cleaned up code

--- embedded/samples/simple_button/src/main.c
+++ embedded/samples/simple_button/src/main.c
@@ -23,26 +23,22 @@ static struct gpio_callback cb2;
 static struct gpio_callback cb3;
 static struct gpio_callback cb4;
 
-bool button_1_state = true;
-bool button_2_state = true;
-bool switch_1_state = true;
-bool switch_2_state = true;
+static bool button_1_state = true;
+static bool button_2_state = true;
+static bool switch_1_state = true;
+static bool switch_2_state = true;
 
-// const struct device *dev, struct gpio_callback *cb, uint32_t pins
-
-void state_change() {
+void state_change() 
+{
 	// button_1_state = gpio_pin_get(dev, button_1);
 	// button_2_state = gpio_pin_get(dev, button_2);
-
 	// switch_1_state = gpio_pin_get(dev, switch_1);
 	// switch_2_state = gpio_pin_get(dev, switch_2);
 
-	// gpio_pin_set(dev, LED_1, switch_1_state);
-	// gpio_pin_set(dev, LED_2, switch_2_state);
-	// gpio_pin_set(dev, LED_3, button_1_state);
-	// gpio_pin_set(dev, LED_4, button_2_state);
+	if(gpio_pin_get(dev, button_1)){gpio_pin_toggle(dev, LED_3);}
+	if(gpio_pin_get(dev, button_2)){gpio_pin_toggle(dev, LED_4);}
 
-	printk("Button 1: %d  Button 2: %d  Switch 1: %d  Switch 2: %d\n\n", button_1_state, button_2_state, switch_1_state, switch_2_state);
+	// printk("Button 1: %d  Button 2: %d  Switch 1: %d  Switch 2: %d\n\n", button_1_state, button_2_state, switch_1_state, switch_2_state);
 }
 void get_state() {
 	button_1_state = gpio_pin_get(dev, button_1);
@@ -53,16 +49,11 @@ void get_state() {
 
 	gpio_pin_set(dev, LED_1, switch_1_state);
 	gpio_pin_set(dev, LED_2, switch_2_state);
-	gpio_pin_set(dev, LED_3, button_1_state);
-	gpio_pin_set(dev, LED_4, button_2_state);
 }
 
 void main(void)
 {
 	printk("\n\n\n\n\n\n\n*** Running Simple Sample ***\n");
-
-	
-	int count = 0;
 
 	dev = device_get_binding("GPIO_0");
 
@@ -86,7 +77,7 @@ void main(void)
 	gpio_pin_configure(dev, switch_2, (GPIO_PULL_UP | GPIO_ACTIVE_LOW));
 	gpio_pin_interrupt_configure(dev, switch_2, GPIO_INT_EDGE_TO_ACTIVE);
 
-
+	// callback setup
 	gpio_init_callback(&cb1, state_change, BIT(switch_1));
 	gpio_init_callback(&cb2, state_change, BIT(switch_2));
 	gpio_init_callback(&cb3, state_change, BIT(button_1));
@@ -97,25 +88,10 @@ void main(void)
 	gpio_add_callback(dev, &cb3);
 	gpio_add_callback(dev, &cb4);
 
-	printk("Set up button at pin %d\n", button_1);
-
-
-	while (1) {
-
-		/* Set pin to HIGH/LOW every 1 second */
-        // if(count % 2 == 0)
-        // {
-        //     gpio_pin_toggle(dev, LED_2);	
-		//     gpio_pin_toggle(dev, LED_3);
-
-			
-
-        // }
-
-		// count++;
-		get_state();
+	while (1) 
+	{
+	    get_state();
 		k_msleep(SLEEP_TIME);
-
 
 	}
 }
