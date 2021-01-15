a06d9b861c33b908ea0fb16ffd103f941b42b660
einatomter <55589561+einatomter@users.noreply.github.com>
2021-01-15 14:36:06 +0100

    leds match switches/buttons

--- embedded/samples/simple_button/src/main.c
+++ embedded/samples/simple_button/src/main.c
@@ -3,7 +3,7 @@
 #include <drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME 	1000
+#define SLEEP_TIME 	1
 
 #define LED_1 2
 #define LED_2 3
@@ -20,6 +20,8 @@ const struct device *dev;
 
 static struct gpio_callback cb1;
 static struct gpio_callback cb2;
+static struct gpio_callback cb3;
+static struct gpio_callback cb4;
 
 bool button_1_state = true;
 bool button_2_state = true;
@@ -28,7 +30,21 @@ bool switch_2_state = true;
 
 // const struct device *dev, struct gpio_callback *cb, uint32_t pins
 
-void button1_pressed() {
+void state_change() {
+	// button_1_state = gpio_pin_get(dev, button_1);
+	// button_2_state = gpio_pin_get(dev, button_2);
+
+	// switch_1_state = gpio_pin_get(dev, switch_1);
+	// switch_2_state = gpio_pin_get(dev, switch_2);
+
+	// gpio_pin_set(dev, LED_1, switch_1_state);
+	// gpio_pin_set(dev, LED_2, switch_2_state);
+	// gpio_pin_set(dev, LED_3, button_1_state);
+	// gpio_pin_set(dev, LED_4, button_2_state);
+
+	printk("Button 1: %d  Button 2: %d  Switch 1: %d  Switch 2: %d\n\n", button_1_state, button_2_state, switch_1_state, switch_2_state);
+}
+void get_state() {
 	button_1_state = gpio_pin_get(dev, button_1);
 	button_2_state = gpio_pin_get(dev, button_2);
 
@@ -39,8 +55,6 @@ void button1_pressed() {
 	gpio_pin_set(dev, LED_2, switch_2_state);
 	gpio_pin_set(dev, LED_3, button_1_state);
 	gpio_pin_set(dev, LED_4, button_2_state);
-
-	printk("Button 1: %d  Button 2: %d  Switch 1: %d  Switch 2: %d\n\n", button_1_state, button_2_state, switch_1_state, switch_2_state);
 }
 
 void main(void)
@@ -53,8 +67,10 @@ void main(void)
 	dev = device_get_binding("GPIO_0");
 
 	/* Set LED pin as output */
+	gpio_pin_configure(dev, LED_1, GPIO_OUTPUT);
 	gpio_pin_configure(dev, LED_2, GPIO_OUTPUT);
 	gpio_pin_configure(dev, LED_3, GPIO_OUTPUT);
+	gpio_pin_configure(dev, LED_4, GPIO_OUTPUT);
 
 	/* Set Button as input */
 	gpio_pin_configure(dev, button_1, (GPIO_PULL_UP | GPIO_ACTIVE_LOW));
@@ -68,11 +84,18 @@ void main(void)
 	gpio_pin_interrupt_configure(dev, switch_1, GPIO_INT_EDGE_TO_ACTIVE);
 
 	gpio_pin_configure(dev, switch_2, (GPIO_PULL_UP | GPIO_ACTIVE_LOW));
+	gpio_pin_interrupt_configure(dev, switch_2, GPIO_INT_EDGE_TO_ACTIVE);
 
-	gpio_init_callback(&cb1, button1_pressed, BIT(button_1));
-	gpio_init_callback(&cb2, button1_pressed, BIT(switch_1));
+
+	gpio_init_callback(&cb1, state_change, BIT(switch_1));
+	gpio_init_callback(&cb2, state_change, BIT(switch_2));
+	gpio_init_callback(&cb3, state_change, BIT(button_1));
+	gpio_init_callback(&cb4, state_change, BIT(button_2));
+
 	gpio_add_callback(dev, &cb1);
 	gpio_add_callback(dev, &cb2);
+	gpio_add_callback(dev, &cb3);
+	gpio_add_callback(dev, &cb4);
 
 	printk("Set up button at pin %d\n", button_1);
 
@@ -90,7 +113,8 @@ void main(void)
         // }
 
 		// count++;
-		// k_msleep(SLEEP_TIME);
+		get_state();
+		k_msleep(SLEEP_TIME);
 
 
 	}
