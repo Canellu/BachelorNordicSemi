4be41fa251b2f65a88bb353ff0780ae714e9d912
Anton Vo <S300938@oslomet.no>
2021-03-18 19:25:33 +0100

    some pages made

---
+++ web/web_v0/public/js/footer.js
@@ -0,0 +1,5 @@
+document.write(`<footer class="bg-gray-800 text-gray-200 h-16 w-full">
+ <div class="m-auto container flex items-center h-full px-10">
+   <h2>&copy; Copyright, designed by Einar</h2>
+ </div>
+</footer>`);
---
+++ web/web_v0/public/js/main.js
@@ -0,0 +1,20 @@
+var dataTabBtn = document.querySelector("#data-tab-btn");
+var dataTab = document.querySelector("#data-tab");
+var controlTabBtn = document.querySelector("#control-tab-btn");
+var controlTab = document.querySelector("#control-tab");
+
+dataTabBtn.addEventListener("click", () => {
+  dataTab.classList.remove("hidden");
+  controlTab.classList.add("hidden");
+
+  dataTabBtn.classList.add("activeTabBtn");
+  controlTabBtn.classList.remove("activeTabBtn");
+});
+
+controlTabBtn.addEventListener("click", () => {
+  controlTab.classList.remove("hidden");
+  dataTab.classList.add("hidden");
+
+  controlTabBtn.classList.add("activeTabBtn");
+  dataTabBtn.classList.remove("activeTabBtn");
+});
