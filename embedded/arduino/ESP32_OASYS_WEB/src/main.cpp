66b7ec68d5e20e619f5fe62706e3d27c2cf4148f
Anton Vo <S300938@oslomet.no>
2021-04-20 19:52:07 +0200

    cleaned up platformio folder

--- embedded/arduino/ESP32_OASYS_WEB/src/main.cpp
+++ embedded/arduino/ESP32_OASYS_WEB/src/main.cpp
@@ -1,10 +1,48 @@
 #include <Arduino.h>
 #include <SPIFFS.h>
+#include "ESPAsyncWebServer.h"
 
-void setup() {
-  // put your setup code here, to run once:
+// --------------------------------------------------
+// Variable declarations
+// --------------------------------------------------
+
+// Wifi
+#ifndef STASSID
+#define STASSID "OASYS-ESP"
+#define STAPSK "aaaaaaaa"
+#endif
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+
+AsyncWebServer server(80);
+
+// --------------------------------------------------
+// Initialize file system
+// --------------------------------------------------
+void initSPIFFS()
+{
+  Serial.println("Initializing SPIFFS");
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  else
+  {
+    Serial.println("SPIFF Initialized");
+  }
 }
 
-void loop() {
+// Setup
+void setup()
+{
+  initSPIFFS();
+  Serial.begin(115200);
+  Serial.println("Hello");
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
