ebcd85551af87b9c2cf8297cfcc477efe94f37c6
Anton Vo <S300938@oslomet.no>
2021-05-12 23:47:43 +0200

    tiny cleanup

--- web/web_v0/public/js/database.js
+++ web/web_v0/public/js/database.js
@@ -1,7 +1,5 @@
 // db is declared in firebase.js
 
-const gliderUID = new URLSearchParams(window.location.search).get("gliderUID");
-
 // Returns mission documents
 function getMissionDocs() {
   return db.collection("Gliders").doc(gliderUID).collection("Missions").get();
