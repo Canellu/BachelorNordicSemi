f60033ceadb1616a9c48f1c803409465f01dd648
Anton Vo <S300938@oslomet.no>
2021-05-18 00:32:28 +0200

    user auto-login needs fix

--- web/web_v0/public/js/index.js
+++ web/web_v0/public/js/index.js
@@ -12,17 +12,6 @@ toLogin.addEventListener("click", (e) => {
   cardContent.classList.remove("flipCard");
 });
 
-// Check if user is logged in and redirect them
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    location.replace("home.html");
-    console.log("Is logged in!");
-  } else {
-    // User is logged out
-    console.log("Is logged out!");
-  }
-});
-
 function validateLogIn(errorMsg) {
   let errorDiv = document.querySelector("#loginErrorMessage");
   errorDiv.innerHTML = errorMsg;
