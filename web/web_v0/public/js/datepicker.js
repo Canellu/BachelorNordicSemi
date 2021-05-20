da5318add5d6c98dad9a899d2d6a25dbd2385a62
Anton Vo <S300938@oslomet.no>
2021-05-21 00:48:40 +0200

    if test had invalid condition

--- web/web_v0/public/js/datepicker.js
+++ web/web_v0/public/js/datepicker.js
@@ -124,19 +124,18 @@ async function applySelection() {
         .get();
 
       let sensorDocs = sensorData.docs;
-      
-      if(sensorDocs != []) {
+
+      if (sensorDocs.length != 0) {
         missionStartDate = parseInt(sensorDocs[0].id.replaceAll("-", ""));
         missionEndDate = parseInt(
           sensorDocs[sensorDocs.length - 1].id.replaceAll("-", "")
         );
-  
+
         if (!(endDate < missionStartDate) && !(missionEndDate < startDate)) {
           missionList.push(mission.id);
         }
       }
 
-     
       count++;
       if (count == missions.size) {
         if (missionList.length) {
@@ -173,7 +172,6 @@ async function applySelection() {
       });
     }
   }
-
 }
 
 // get data within specified range, only checks data within passed mission
